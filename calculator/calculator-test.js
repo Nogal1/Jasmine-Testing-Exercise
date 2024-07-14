15473694cacd8d813badec05606639b4d44682bd
Nogal <logan.mccl0406@gmail.com>
2024-07-14 10:39:08 -0700

    working calc and calc test

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,21 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount: 10000,
+    years: 10,
+    rate: 12.5,
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('146.38')
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 10000,
+    years: 8,
+    rate: 8.0
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('141.37')
 });
 
 /// etc
